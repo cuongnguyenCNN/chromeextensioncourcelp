36786cf65401d138b2a81897fa3577a4fc3c4030
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2025-04-20 15:28:01 +0700

    agaga

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" items-center">
       <main className="min-h-screen bg-gradient-to-br from-[#0f172a] to-[#1e293b] text-white font-sans">
         <section className="text-center py-24 px-6">
           <h1 className="text-5xl md:text-6xl font-extrabold mb-6 leading-tight">
